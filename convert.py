ac4914330dd1a9c0193318369e026524ea6135df
Francesco Gigli <jaramir@gmail.com>
2013-02-25 22:37:56 +0000

    Added reports_to field

--- convert.py
+++ convert.py
@@ -7,11 +7,19 @@ import json
 
 per_strength = {}
 per_person = defaultdict(list)
+reports_to = {}
+
+def clean(value):
+    return re.sub("[12345\(\) ]", "", value).strip()
 
 # read
 with open("strength_wiki.csv", "r") as src:
     for row in csv.reader(src):
-        per_strength[row[0].strip()] = [re.sub("[12345\(\) ]", "", x).strip() for x in row[1:] if x != ""]
+        per_strength[clean(row[0])] = [clean(x) for x in row[1:] if x != ""]
+
+with open("reports_to.csv", "r") as src:
+    for row in csv.reader(src):
+        reports_to[clean(row[0])] = clean(row[1])
 
 # rotate
 for strength, people in per_strength.items():
@@ -23,6 +31,7 @@ data = []
 for person, strengths in sorted(per_person.items()):
     data.append( {
             "name": person,
+            "reports_to": reports_to[person],
             "strengths": sorted(strengths)
             } )
 
